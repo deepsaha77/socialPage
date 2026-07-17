94ddaf24174b905bb648b0eecac077e327daa2b4
Deep Saha <sahadeep171@gmail.com>
2026-07-17 23:29:48 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const User = require("./models/User");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/socialbook")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/login", async (req, res) => {
+
+    const { username, password } = req.body;
+
+    const user = new User({
+        username,
+        password
+    });
+
+    await user.save();
+    res.send("<h2>Server is down please try later!</h2>");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
